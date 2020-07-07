efbd23a8ff51754d3caf32ec78602625e6d78c71
denchech <d_cher00@mail.ru>
2020-07-07 16:40:47 +0500

    Add resizing of image

--- src/Service/TextConverter.php
+++ src/Service/TextConverter.php
@@ -10,6 +10,7 @@ class TextConverter
 {
     private const SIZE = 14;
     private const ANGLE = 0;
+    private const MAX_LETTERS_IN_STRING = 55;
 
     private $font;
     private $imagesDirectory;
@@ -24,6 +25,7 @@ class TextConverter
 
     public function convert(string $text): string
     {
+        $text = self::splitText($text);
         $size = imagettfbbox(self::SIZE, self::ANGLE, $this->font, $text);
         $xSize = abs($size[0]) + abs($size[2]);
         $ySize = abs($size[5]) + abs($size[1]);
@@ -69,5 +71,39 @@ class TextConverter
             'uuid' => $uuid,
             'filepath' => $filepath
         ];
+    }
+
+    private static function splitText(string $text): string
+    {
+        $newText = "";
+        $text = preg_replace('/[ ]+/', ' ', $text);
+        $splitTextArray = explode("\n", $text);
+        foreach ($splitTextArray as $str) {
+            $trimStr = trim($str, " ");
+            if ($trimStr === "\n" || $trimStr === "\r") {
+                $newText .= "\n\n";
+                continue;
+            }
+            $splitString = explode(' ', $str);
+            $currentStr = "";
+            foreach ($splitString as $word) {
+                $word .= " ";
+                if (strlen($currentStr) + strlen($word) < self::MAX_LETTERS_IN_STRING) {
+                    $currentStr .= $word;
+                } else {
+                    if (strlen($word) > self::MAX_LETTERS_IN_STRING) {
+                        $newText .= $currentStr . "\n" . substr($word, 0, self::MAX_LETTERS_IN_STRING);
+                        $currentStr = "\n" . substr($word, self::MAX_LETTERS_IN_STRING - strlen($word));
+                    } else {
+                        $newText .= $currentStr;
+                        $currentStr = "\n" . $word;
+                    }
+                }
+            }
+            $newText .= trim($currentStr, ' ');
+        }
+        $newText = preg_replace('/\r+/', "\n", $newText);
+        $newText = preg_replace('/[\n]{3,}/', "\n\n", $newText);
+        return $newText;
     }
 }
